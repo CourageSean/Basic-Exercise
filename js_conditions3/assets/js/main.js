c0b98bb69fcef96caff2db3ee46ca25407adf50e
CourageSean <seancourage23@gmail.com>
2021-02-11 14:20:22 +0100

    g

---
+++ js_conditions3/assets/js/main.js
@@ -0,0 +1,11 @@
+let pop1 = document.getElementById("pop1");
+let pop2 = document.getElementById("pop2");
+let pop3 = document.getElementById("pop3");
+
+function info() {
+  pop1.style.display = "none"
+    ? (pop1.style.display = "block")
+    : (pop1.style.display = "none");
+
+  // let isHidden = true
+}
